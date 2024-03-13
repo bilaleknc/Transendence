f927ff0333911526c561116122db61baf3aaf401
Mustafa Eren Erdoğan <mustafaerenerdogan@ip-192-168-1-35.eu-west-1.compute.internal>
2024-03-13 14:52:35 +0300

    [CHAT] update

--- chat/static/chat/js/app.js
+++ chat/static/chat/js/app.js
@@ -1,2 +1,9 @@
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
 alert("roomName: " + roomName);
+
+// Create a WebSocket connection to the server
+const chatSocket = new WebSocket(
+    'ws://' + window.location.host +
+    '/ws/chat/' + roomName + '/');
+
+    
