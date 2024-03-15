89317db858994b46d03cd86cd6537a5868ed47c2
Bilal EKİNCİ <etkhckrone@gmail.com>
2024-03-15 22:54:08 +0300

    [LOGIN && FRONT] Add SSL certificates for HTTPS support and update Dockerfile and nginx.conf

--- frontend/Components/Navbar.js
+++ frontend/Components/Navbar.js
@@ -1,46 +1,56 @@
 class Navbar extends HTMLElement {
- constructor() {
-    super();
-    this.innerHTML = `
-    <nav class="navbar navbar-expand-xl navbar-dark bg-dark border-bottom">
-    <a class="navbar-brand" href="/">
-        <img src="../Public/logo.png" class="p-1 mx-3" width="120" height="50" alt="">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse navbarNavAltMarkup">
-        <form class="mr-3" style="width: 250px;">
-            <input type="search" class="form-control rounded" placeholder="Find a friend"
-            aria-label="Search" aria-describedby="search-addon" />
-        </form>
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/game">Game</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/live-chat">Live Chat</a>
-            </li>
-            <li class="nav-item dropdown">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="bi bi-person"></span>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="/sign-in">Sign In</a>
-                        <a class="dropdown-item" href="/sign-up">Sign Up</a>
-                    </div>
-                </li>
-            </li>
-        </ul>
-    </div>
-    </div>
-</nav>
-    `
- }   
-}
-
-customElements.define('my-navbar', Navbar);
+	constructor() {
+	  super();
+	  this.innerHTML = `
+	  <nav class="navbar navbar-expand-xl navbar-dark bg-dark border-bottom">
+	  <a class="navbar-brand" href="/">
+		  <img src="../Public/logo.png" class="p-1 mx-3" width="120" height="50" alt="">
+	  </a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse navbarNavAltMarkup">
+		  <form class="mr-3" style="width: 250px;">
+			  <input type="search" class="form-control rounded" placeholder="Find a friend"
+			  aria-label="Search" aria-describedby="search-addon" />
+		  </form>
+		  <ul class="navbar-nav">
+			  <li class="nav-item active">
+				  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				  <a class="nav-link" href="/game">Game</a>
+			  </li>
+			  <li class="nav-item">
+				  <a class="nav-link" href="/live-chat">Live Chat</a>
+			  </li>
+			  <li class="nav-item dropdown">
+				  <li class="nav-item dropdown">
+					  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					  <span class="bi bi-person"></span>
+					  </a>
+					  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						  <a class="dropdown-item" href="#" id="signInLink">Sign In</a>
+						  <a class="dropdown-item" href="/sign-up">Sign Up</a>
+					  </div>
+				  </li>
+			  </li>
+		  </ul>
+	  </div>
+	  </div>
+	  </nav>
+	  `;
+	}
+  
+	connectedCallback() {
+	  this.querySelector('#signInLink').addEventListener('click', (e) => {
+		e.preventDefault();
+		// Burada OAuth işlemi için gerekli yönlendirme yapılabilir
+		// Örneğin:
+		window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d18dddbdb080ff4297c863cacf173408025c2f1205a01ca72c0346749d360b59&redirect_uri=https%3A%2F%2F127.0.0.1%3A8080%2F42api%2F&response_type=code";
+	});
+	}
+  }
+  
+  customElements.define('my-navbar', Navbar);
+  
---
+++ login/login/views.py
@@ -0,0 +1,40 @@
+import requests
+from django.http import JsonResponse
+from django.conf import settings
+
+def login_via_42(request):
+    print("l!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ogin_via_42 code")
+    print(code)
+    
+    code = request.GET.get('code')
+    print('codeeeeeeeeeeeeeeeeeee', code)
+    if not code:
+        return JsonResponse({'error': 'Code parametresi eksik'}, status=400)
+
+    # Access token almak için OAuth bilgilerinizi kullanın
+    token_url = "https://api.intra.42.fr/oauth/token"
+    redirect_uri = "https://127.0.0.1:8082/42api/"
+    client_id = "u-s4t2ud-0e9b5b9ed0f2110a0b29d40e22bf8249ca7f4c31681ac4a2c1582d7a5b0c790a"
+    client_secret = "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" # send to settings.py
+    payload = {
+        'grant_type': 'authorization_code',
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': code,
+        'redirect_uri': redirect_uri
+    }
+
+    response = requests.post(token_url, data=payload)
+    if response.status_code == 200:
+        token_data = response.json()
+        access_token = token_data.get('access_token')
+        
+        # İsteğe bağlı: Access token ile kullanıcı bilgilerini al
+        # user_info_url = "https://api.intra.42.fr/v2/me"
+        # headers = {'Authorization': f'Bearer {access_token}'}
+        # user_response = requests.get(user_info_url, headers=headers)
+        # user_data = user_response.json()
+
+        return JsonResponse({'message': 'Başarılı giriş', 'access_token': access_token})
+    else:
+        return JsonResponse({'error': 'Token alınamadı'}, status=response.status_code)
