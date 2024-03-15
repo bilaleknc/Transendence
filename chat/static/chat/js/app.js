c4b8a20b4efa19425822713e293708241ab38cc4
Mustafa Eren Erdoğan <mustafaerenerdogan@ip-192-168-1-41.eu-west-1.compute.internal>
2024-03-15 22:50:35 +0300

    [CHAT] live chat backend connection

--- chat/static/chat/js/app.js
+++ chat/static/chat/js/app.js
@@ -1,9 +1,7 @@
-const roomName = JSON.parse(document.getElementById('room-name').textContent);
+const roomName = JSON.parse(document.getElementById("room-name").textContent);
 alert("roomName: " + roomName);
 
 // Create a WebSocket connection to the server
 const chatSocket = new WebSocket(
-    'ws://' + window.location.host +
-    '/ws/chat/' + roomName + '/');
-
-    
+  "ws://" + window.location.host + "/ws/chat/" + roomName + "/"
+);
