0617f015821d02178341f7110670cdf3eb65ebd5
süleyman akkuş <seyitakkus44@gmail.com>
2024-03-17 04:17:05 +0300

    [Devops] dev-bilal branch and dev-suleyman branch merged.

--- backend/game/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-	    path('ws/socket-server/<str:room_name>/', consumers.GameConsumer.as_asgi()),
-] 
--- client/app.js
+++
@@ -1,8 +0,0 @@
-import HTMLTemplate from "./ModulesJS/HTMLTemplate.js";
-import "./ModulesJS/SPA.js";
-import "./Components/UsAbout.js"
-import "./Components/Footer.js"
-import "./Components/Navbar.js"
-import "./Components/Game.js"
-
-
