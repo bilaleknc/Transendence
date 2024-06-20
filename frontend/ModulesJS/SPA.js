e8089aad3dbccae7d2a7c0fccb61c70514f37cc3
Mustafa Eren Erdoğan <mustafaerenerdogan@ip-192-168-1-34.eu-west-1.compute.internal>
2024-06-20 12:12:14 +0300

    delete live chat form navbar

--- frontend/ModulesJS/SPA.js
+++ frontend/ModulesJS/SPA.js
@@ -17,11 +17,6 @@ class SPA {
       title: "game",
       description: "Oyun Sayfası",
     },
-    "/live-chat": {
-      template: "/templates/live-chat.html",
-      title: "live-chat",
-      description: "Canlı mesajlaşma",
-    },
     "/sign-up": {
       template: "/templates/sign-up.html",
       title: "sign-up",
@@ -71,6 +66,16 @@ class SPA {
       if (!accessToken) {
         alert("Oyun oynamak için giriş yapmalısınız.");
         location = "/";
+        window.history.pushState({}, "", location);
+        return false;
+      }
+    }
+    if (location == "/profile") {
+      const accessToken = localStorage.getItem("access_token");
+      if (!accessToken) {
+        alert("Profil sayfasını görmek için giriş yapmalısınız.");
+        location = "/";
+        window.history.pushState({}, "", location);
         return false;
       }
     }
