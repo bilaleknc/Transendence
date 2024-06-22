d420bf324454f443d3cb12ca2521bdf2aa32da14
Mustafa Eren Erdoğan <mustafaerenerdogan@Mustafas-MacBook-Pro.local>
2024-06-22 15:11:12 +0300

    restart

--- frontend/nginx_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/certs/localhost.key -out /etc/nginx/certs/localhost.crt -subj "/C=TR/ST=Istanbul/L=Istanbul/O=local/CN=pingpong";
-echo "Nginx: ssl is set up!";
-fi
-
-exec "$@"
--- user/user/apps.py
+++
@@ -1,9 +0,0 @@
-# apps.py
-from django.apps import AppConfig
-
-class UserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user'
-
-    def ready(self):
-        import user.signals
